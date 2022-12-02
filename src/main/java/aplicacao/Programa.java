01ec9920e703075bd203de7fd947d5f74bf575e2
Pablo Tokuo <pbo_1996@hotmail.com>
2022-12-02 11:47:55 -0300

    Deletendo do banco de dados e Entidade Monitorada

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -13,8 +13,18 @@ public class Programa {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
         EntityManager em = emf.createEntityManager();
 
-        Pessoa p = em.find(Pessoa.class, 2);
-        System.out.println(p);
+//        Exemplo de entidade monitorada
+//        Como ja tinha sido inserida no banco de dados
+//        Precisou recuperar(procurar)
+        Pessoa p = em.find(Pessoa.class, 4);
+//        E como não era uma simples leitura do banco de dados
+//        Preciso iniciar uma transação
+        em.getTransaction().begin();
+//        Metodo para remover a pessoa
+        em.remove(p);
+//        E commitando a operacao
+        em.getTransaction().commit();
+
         System.out.println("Pronto!");
 
         em.close();
