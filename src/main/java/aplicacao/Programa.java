52cd3c3bb2b2df3e24e885868be3012e9b4c9e3e
Pablo Tokuo <pbo_1996@hotmail.com>
2022-12-02 11:42:12 -0300

    Procurar pessoa no banco de dados

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -10,26 +10,14 @@ import javax.persistence.Persistence;
 public class Programa {
     public static void main(String[] args) {
 
-//        Proprio banco de dados vai inserir as pessoas no banco
-//        Por isso ID Null
-
-        Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
-        Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
-        Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
-
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
         EntityManager em = emf.createEntityManager();
 
-//        Como nao é uma leitura do banco de dados
-//        Precisa começar uma Transação
-        em.getTransaction().begin();
-//        Persist que vai inserir o obj no banco de dados
-        em.persist(p1);
-        em.persist(p2);
-        em.persist(p3);
-//        Apos finalizar commitar as mudancas
-        em.getTransaction().commit();
+        Pessoa p = em.find(Pessoa.class, 2);
+        System.out.println(p);
         System.out.println("Pronto!");
-        
+
+        em.close();
+        emf.close();
     }
 }
